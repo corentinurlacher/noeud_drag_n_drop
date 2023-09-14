6c9b8de170f6f081ad3eeeb5a858ecaca782a967
Corentin Jean-Luc Nicolas Urlacher <2130315@etu.cegepjonquiere.ca>
2023-09-14 15:11:41 -0400

    init

---
+++ Components/Noeud.cs
@@ -0,0 +1,22 @@
+﻿using static BlazorDraggableDemo.Components.Connector;
+
+namespace BlazorDraggableDemo.Components
+{
+    public class Noeud
+    {
+        public int IdNoeud;
+        public Noeud? Parent;
+        public double X = 50;
+        public double Y = 50;
+        public double R = 40;
+        public Direction Dir;
+        public double RootX;
+        public double RootY;
+        public string Link = @"/";
+        public string Name = @"Noeud";
+
+        public string Fill = "#333";
+        public double ConnectY => Dir == Direction.Top ? Y - R : Y + R;
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+using BlazorDraggableDemo;
+using BlazorDraggableDemo.Components;
+using BlazorDraggableDemo.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => 
+	new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services
+	.AddSingleton<MouseService>()
+	.AddSingleton<IMouseService>(ff => ff.GetRequiredService<MouseService>());
+
+await builder.Build().RunAsync();
