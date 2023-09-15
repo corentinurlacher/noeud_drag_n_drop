ea0143e737d620b87d72b6056642469d01c3d9bc
Marc-Antoine Bouchard <1933191@etu.cegepjonquiere.ca>
2023-09-15 10:29:57 -0400

    bd

---
+++ Modeles/Graphe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Capsule_Charles_Marc_Antoine.Modeles
+{
+    public partial class Graphe
+    {
+        public Graphe()
+        {
+            Noeuds = new HashSet<Noeud>();
+        }
+
+        public int Id { get; set; }
+        public string Nom { get; set; } = null!;
+        public string Statut { get; set; } = null!;
+
+        public virtual ICollection<Noeud> Noeuds { get; set; }
+    }
+}
